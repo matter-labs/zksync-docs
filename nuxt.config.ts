c10d086a31df770db17d30a2e6d550f4b54e1bba
Sabrina <brina.heath@gmail.com>
2024-03-19 08:24:14 -0700

    Init nuxt content (#4)
    # What :computer: 
    * Setup Nuxt Content project with a starter template that includes Nuxt,
    Nuxt Content, Nuxt UI Pro, Nuxt Studio.
    * Modified setting configs for variety of things. Prettier, ESLint,
    MarkdownLint, Spellcheck, etc.
    * Threw in all the existing markdown documentation from existing site
    into `/content/`. Will figure out cleanup and organization in the
    process.
    
    # Why :hand:
    * Need a new framework to develop in
    * Need to mess with existing docs and how it'll integrate into the new
    framework
    
    
    # Notes :memo:
    README is not up to date for managing and running the project yet.
    There's a bunch of junk also that may make no sense, but was just caught
    up in the commit when testing out the framework.
    
    To get started, install then run `bun run dev -o`
    
    ---------
    
    Co-authored-by: Dustin Brickwood <dustinbrickwood204@gmail.com>

---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/fonts', '@nuxthq/studio', 'nuxt-og-image'],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter((c) => ['UButton', 'UIcon', 'Test'].includes(c.pascalName));
+
+      globals.forEach((c) => (c.global = true));
+    },
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true },
+  },
+  devtools: {
+    enabled: true,
+  },
+  typescript: {
+    strict: false,
+  },
+  content: {
+    highlight: {
+      langs: [
+        'solidity',
+        'go',
+        'jsx',
+        'swift',
+        'python',
+        'rust',
+        'toml',
+        'java',
+        'xml',
+        'graphql',
+        'groovy',
+        'asm',
+        'c++',
+      ],
+      theme: {
+        default: 'github-light',
+        light: 'github-light',
+        dark: 'github-dark',
+        sepia: 'monokai',
+      },
+    },
+  },
+});
