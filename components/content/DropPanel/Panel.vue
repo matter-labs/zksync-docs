3d62e7dd1bfa93d94ef529af3d18b3bf2dd50ecf
Dustin Brickwood <dustinbrickwood204@gmail.com>
2024-04-11 13:53:30 -0500

    feat: remove outdated files, add back components
    chore: remove getting_started default

---
+++ components/content/DropPanel/Panel.vue
@@ -0,0 +1,11 @@
+<template>
+  <ContentSlot :use="$slots.default" />
+</template>
+
+<script setup lang="ts">
+/**
+ * A panel component that can be slotted into a DropPanel.
+ *
+ * Use the `label` prop to set the tab label on the drop panel.
+ */
+</script>
