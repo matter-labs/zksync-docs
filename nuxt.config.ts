dce9eba01010d39e2112e8838ab01cb6dc54f9a3
Sabrina <sf@matterlabs.dev>
2024-05-16 16:27:20 -0400

    chore: update theme (#62)
    # What :computer: 
    * update theme
    
    # Why :hand:
    * latest changes in docs template repo need to be applied.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo'],
   routeRules: {
     '/api/search.json': { prerender: true },
+  },
+  site: {
+    name: 'zkSync Docs',
+    url: process.env.NUXT_SITE_ENV ? 'https://staging-docs.zksync.io' : 'https://docs.zksync.io',
+  },
+  runtimeConfig: {
+    public: {
+      app: 'docs',
+    },
   },
 });
