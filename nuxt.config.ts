549dd329840af32398fcfbf5c32248a56baac727
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-20 18:51:04 -0400

    feat: add sdk docs as part of build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
+  extends: [['github:matter-labs/docs-nuxt-template', { install: true }], ['github:zksync-sdk/sdk-docs#staging']],
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo'],
   routeRules: {
     '/api/search.json': { prerender: true },
@@ -8,6 +8,17 @@ export default defineNuxtConfig({
   site: {
     name: 'zkSync Docs',
     url: process.env.NUXT_SITE_ENV ? 'https://staging-docs.zksync.io' : 'https://docs.zksync.io',
+  },
+  content: {
+    sources: {
+      sdk: {
+        prefix: '/sdk',
+        driver: 'github',
+        repo: 'zksync-sdk/sdk-docs',
+        branch: 'staging',
+        dir: 'content/sdk',
+      },
+    },
   },
   runtimeConfig: {
     public: {
