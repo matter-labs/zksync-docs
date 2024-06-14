d713520543dc0d3da4a44f4e8eaaceab8e5f4a6b
Sabrina <sf@matterlabs.dev>
2024-06-14 18:14:51 -0400

    feat: add PWA to site (#129)
    # Description
    
    Adds PWA functionality to site. Primarily this is to hopefully
    invalidate any old service worker functions from the previous docs
    website.
    
    Adding this PR should update and clear whatever lingering PWA features
    from the old docs site. If we do not want PWA implemented any further,
    we should be able to easily unregister it properly with the following
    guide:
    https://vite-pwa-org.netlify.app/guide/unregister-service-worker.html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [['github:matter-labs/docs-nuxt-template', { install: true }], ['github:zksync-sdk/sdk-docs']],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo', 'nuxt-gtag'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo', 'nuxt-gtag', '@vite-pwa/nuxt'],
   site: {
     name: 'ZKsync Docs',
     url: process.env.NUXT_SITE_ENV ? 'https://staging-docs.zksync.io' : 'https://docs.zksync.io',
@@ -34,6 +34,60 @@ export default defineNuxtConfig({
     '/build/resources': { redirect: '/build/resources/glossary' },
     '/zk-stack/concepts': { redirect: '/zk-stack/concepts/transaction-lifecycle' },
     '/zk-stack/running-a-zk-chain': { redirect: '/zk-stack/running-a-zk-chain/locally' },
+  },
+  pwa: {
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    workbox: {
+      cleanupOutdatedCaches: true,
+      cacheId: 'zksync-docs-prod',
+    },
+    manifest: {
+      name: 'ZKsync Developer Documentation',
+      short_name: 'ZKsync Docs',
+      description:
+        'ZKsync Docs bring you all information you need about our protocol, APIs, SDKs, ZK Stack, and ZK chains. Start with our guides and tutorials, or go deep into our architecture and protocol specification.',
+      theme_color: '#F2F2F2',
+      icons: [
+        {
+          src: '/zksync-icon_48.svg',
+          sizes: '48x48',
+          type: 'image/svg+xml',
+        },
+        {
+          src: '/zksync-icon_48.png',
+          sizes: '48x48',
+          type: 'image/png',
+        },
+        {
+          src: '/zksync-icon_64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: '/zksync-maskable.png',
+          sizes: '128x128',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/zksync-icon_180.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: '/zksync-icon_192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/zksync-icon_512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+      ],
+    },
   },
   $production: process.env.NUXT_SITE_ENV
     ? {}
