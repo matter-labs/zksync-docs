53866e2f0a91055f2eb9c3b707180630ad06869e
Sabrina Ferguson <sf@matterlabs.dev>
2024-08-21 16:04:37 -0400

    fix: search params

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -2,6 +2,6 @@ import { serverQueryContent } from '#content/server';
 
 export default eventHandler(async (event) => {
   return serverQueryContent(event)
-    .where({ _type: 'markdown', navigation: { $ne: false } })
+    .where({ _type: 'markdown', _partial: false, navigation: { $ne: false } })
     .find();
 });
