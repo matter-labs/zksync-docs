549a11bd6f945b004c42b2ce64d7dd734d8140eb
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-04-02 20:52:51 -0500

    chore: update verifier endpoints and evm verification faq

--- examples/hardhat-vyper-template/hardhat.config.ts
+++ examples/hardhat-vyper-template/hardhat.config.ts
@@ -14,14 +14,14 @@ const config: HardhatUserConfig = {
       url: 'https://sepolia.era.zksync.dev',
       ethNetwork: 'sepolia',
       zksync: true,
-      verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification',
+      verifyURL: 'https://block-explorer-api.sepolia.zksync.dev/api',
       accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
     },
     ZKsyncEraMainnet: {
       url: 'https://mainnet.era.zksync.io',
       ethNetwork: 'mainnet',
       zksync: true,
-      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
+      verifyURL: 'https://block-explorer-api.mainnet.zksync.io/api',
       accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
     },
     dockerizedNode: {
