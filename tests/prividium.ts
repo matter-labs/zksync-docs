1eced31877a482e265d1e1e2cf37955675e9d2de
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-06-04 09:13:04 -0600

    fix: fixes

--- tests/prividium.ts
+++ tests/prividium.ts
@@ -62,11 +62,9 @@ const steps: IStepConfig = {
     useSetCommand: 'npm install',
   },
   'add-pk': {
-    action: 'modifyFile',
+    action: 'writeToFile',
     filepath: 'tests-output/prividium/prividium-token/.env',
     useSetData: `WALLET_PRIVATE_KEY=0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110`,
-    atLine: 1,
-    removeLines: [1],
   },
   'change-network': {
     action: 'modifyFile',
