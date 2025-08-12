82228a838b989f32eadc1d9d8f544453aeb09e66
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-08-12 14:03:51 -0600

    docs: update migration guide

---
+++ examples/101/deploy/3-proxy-contracts/beacon/upgrade.ts
@@ -0,0 +1,37 @@
+// ANCHOR: upgrade
+import { ethers, upgrades } from 'hardhat';
+import type { V2_BeaconCrowdfundingCampaign } from '../../../typechain-types';
+
+// Update with the address for your beacon contract
+// and the beacon proxy address
+const beaconAddress = process.env.BEACON_ADDRESS ?? 'YOUR_BEACON_ADDRESS_HERE';
+const proxyAddress = process.env.BEACON_PROXY_ADDRESS ?? 'YOUR_PROXY_ADDRESS_HERE';
+
+async function main() {
+  const beaconV2Factory = await ethers.getContractFactory('V2_BeaconCrowdfundingCampaign');
+
+  // Upgrade the proxy to V2
+  await upgrades.upgradeBeacon(beaconAddress, beaconV2Factory);
+
+  console.log('Successfully upgraded BeaconCrowdfundingCampaign to V2_BeaconCrowdfundingCampaign');
+
+  const upgradedContract = beaconV2Factory.attach(proxyAddress) as V2_BeaconCrowdfundingCampaign;
+
+  // wait some time before the next call
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // Initialize V2 with a new campaign duration
+  const durationInSeconds = 30 * 24 * 60 * 60; // For example, setting a 30-day duration
+  const initTx = await upgradedContract.initializeV2(durationInSeconds);
+  const receipt = await initTx.wait();
+
+  console.log(`V2_BeaconCrowdfundingCampaign initialized. Transaction Hash: ${receipt?.hash}`);
+}
+// ANCHOR: upgrade
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ examples/101/hardhat.config.ts
@@ -0,0 +1,61 @@
+import type { HardhatUserConfig } from 'hardhat/config';
+
+import '@nomicfoundation/hardhat-toolbox';
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-chai-matchers';
+import '@matterlabs/hardhat-zksync';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'dockerizedNode',
+  networks: {
+    // ANCHOR: verify
+    ZKsyncEraSepolia: {
+      url: 'https://sepolia.era.zksync.dev',
+      ethNetwork: 'sepolia',
+      zksync: true,
+      verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification',
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+    },
+    ZKsyncEraMainnet: {
+      url: 'https://mainnet.era.zksync.io',
+      ethNetwork: 'mainnet',
+      zksync: true,
+      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+    },
+    // ANCHOR_END: verify
+    // ANCHOR: docker-network
+    dockerizedNode: {
+      url: 'http://localhost:3050',
+      ethNetwork: 'http://localhost:8545',
+      zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+    },
+    // ANCHOR_END: docker-network
+    anvilZKsync: {
+      url: 'http://127.0.0.1:8011',
+      ethNetwork: 'http://localhost:8545',
+      zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+    },
+    hardhat: {
+      zksync: true,
+    },
+  },
+  zksolc: {
+    version: '1.5.15',
+    settings: {
+      codegen: 'yul',
+      // find all available options in the official documentation
+      // https://docs.zksync.io/build/tooling/hardhat/hardhat-zksync-solc#configuration
+    },
+  },
+  solidity: {
+    version: '0.8.30',
+  },
+};
+
+export default config;
